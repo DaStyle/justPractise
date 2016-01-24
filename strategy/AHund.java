b50b5465ff8f70497343f11727b5adc9e7793ea0
Da Family <dafamily@Das-MacBook-Pro.fritz.box>
2016-01-24 18:42:22 +0100

    Remove old Files

--- strategy/AHund.java
+++
@@ -1,9 +0,0 @@
-package de.dennis.strategy;
-
-/**
- * Created by dafamily on 24.01.16.
- */
-public abstract class AHund {
-    public abstract String bellen();
-    public abstract String laufen();
-}
